3b5188d8df8faafc0c4649976f74c9fa1a58c3f7
mohammed-muzzammil <muzzammilsilat56@gmail.com>
2020-12-28 19:07:50 +0530

    first commit

---
+++ django_school/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from django.contrib import admin
+from classroom.views import classroom, students, teachers
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+    path('', include('classroom.urls')),
+    # path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
+    path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
